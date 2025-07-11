c72962bacc6beede7a46e9c2cbd6bbe80fac8c5e
tuanminhh03 <142196223+tuanminhh03@users.noreply.github.com>
2025-07-12 00:59:15 +0700

    Simplify user dropdown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
             </HeaderWrapper>
           }
         />
-        <Route 
+        <Route
           path="/my-orders"
           element={
             <HeaderWrapper>
