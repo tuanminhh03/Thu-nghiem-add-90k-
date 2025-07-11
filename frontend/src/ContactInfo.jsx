899a3f0f06595f6ca5b05047e5ef5ef9f04f3555
tuanminhh03 <tuanminhh203@gmail.com>
2025-07-12 01:48:59 +0700

    update text for contact

--- frontend/src/ContactInfo.jsx
+++ frontend/src/ContactInfo.jsx
@@ -5,14 +5,14 @@ export default function ContactInfo() {
   return (
     <div className="contact-info">
       <div className="contact-item">
-        <img src="/images/zalo-icon.svg" alt="Zalo" className="contact-icon" />
+        <img src="/public/images/zalo-icon.svg" alt="Zalo" className="contact-icon" />
         <div className="contact-text">
           <span className="contact-name">Chat Zalo</span>
           <span className="contact-hours">(8h-24h)</span>
         </div>
       </div>
       <div className="contact-item">
-        <img src="/images/messenger-icon.svg" alt="Messenger" className="contact-icon" />
+        <img src="./public/images/messenger-icon.svg" alt="Messenger" className="contact-icon" />
         <div className="contact-text">
           <span className="contact-name">Chat Messenger</span>
           <span className="contact-hours">(8h-24h)</span>
